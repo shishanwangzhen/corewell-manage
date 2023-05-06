69c09a6c73e75e0812da7ec4c944235476a39153
Administrator <https://github.com/shishanwangzhen/corewell-study.git>
2023-05-06 11:55:22 +0800

    init

---
+++ src/main/java/com/corewell/corewellmanage/CorewellManageApplication.java
@@ -0,0 +1,16 @@
+package com.corewell.corewellmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class CorewellManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorewellManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/corewell/corewellmanage/CorewellManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corewell.corewellmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorewellManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
