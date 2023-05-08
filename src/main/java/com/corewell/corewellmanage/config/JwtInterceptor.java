f47616f8ddb3bd4ebbf76535b69a8b51c9aa0500
Administrator <https://github.com/shishanwangzhen/corewell-study.git>
2023-05-08 10:50:46 +0800

    config

---
+++ src/main/java/com/corewell/corewellmanage/config/JwtInterceptor.java
@@ -0,0 +1,48 @@
+package com.corewell.corewellmanage.config;
+
+
+import com.corewell.corewellmanage.result.ResultStatusCode;
+import com.corewell.corewellmanage.utils.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * @author Administrator
+ */
+@Slf4j
+public class JwtInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //如果不是映射到方法直接通过
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+
+        //从 http 请求头中取出 token
+        log.info(request.getRequestURI());
+        String token = request.getHeader("token");
+        log.info("此处测试是否拿到了token：" + token);
+        if (token == null) {
+            throw new RuntimeException(ResultStatusCode.TOKEN_IS_NULL.getErrmsg());
+        }
+
+        //验证 token
+        JwtUtil.checkSign(token);
+
+        //验证通过后， 这里测试取出JWT中存放的数据
+        //获取 token 中的 userId
+        String userId = JwtUtil.getUserId(token);
+        log.info("userId : " + userId);
+
+        //获取 token 中的其他数据
+        Map<String, Object> info = JwtUtil.getInfo(token);
+        info.forEach((k, v) -> log.info(k + ":" + v));
+        return true;
+    }
+}
+
---
+++ src/main/java/com/corewell/corewellmanage/config/MvcConfig.java
@@ -0,0 +1,39 @@
+package com.corewell.corewellmanage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ * @author Administrator
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 添加jwt拦截器，并指定拦截路径
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //TODO 测试环境关闭token检验
+       /*  if (true) {
+            return;
+        }*/
+        registry.addInterceptor(jwtInterceptor())
+                //添加拦截
+                .addPathPatterns("/core/**");
+        //放开拦截
+        //.excludePathPatterns("/services","/username");
+    }
+
+    /**
+     * jwt拦截器
+     */
+    @Bean
+    public JwtInterceptor jwtInterceptor() {
+        return new JwtInterceptor();
+    }
+}
+
