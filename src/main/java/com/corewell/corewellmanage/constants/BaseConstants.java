c8be14382f185238ec1d28f7dbae0701b11c7e41
Administrator <https://github.com/shishanwangzhen/corewell-study.git>
2023-05-08 10:48:51 +0800

    constants

---
+++ src/main/java/com/corewell/corewellmanage/constants/BaseConstants.java
@@ -0,0 +1,42 @@
+package com.corewell.corewellmanage.constants;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: wangzhen
+ * @Date: 2022/11/30/9:51
+ * @Description:
+ */
+public class BaseConstants {
+    /**
+     * TLINK还参成功码
+     */
+    public static final String SUCCESS_00 = "00";
+
+    /**
+     * TLINK还参码无数据
+     */
+    public static final String SUCCESS_01 = "01";
+
+    /**
+     * TLINK接口token异常
+     */
+    public static final String INVALID_TOKEN = "invalid_token";
+
+    /**
+     * 手机号正则表达式
+     */
+
+    public static final String REG_PATTERN_PHONE = "(^$)|(^(1)[0-9]{10}$)";
+
+    /**
+     * 江苏农林学号正则表达式
+     */
+    //public static final String REG_PATTERN_ACCOUNT = "^(20)[0-9]{10}$";
+
+    /**
+     * 南京农业学号正则表达式
+     */
+    public static final String REG_PATTERN_ACCOUNT = "^[0-9]*$";
+
+}
---
+++ src/main/java/com/corewell/corewellmanage/constants/BaseRedisKeyConstants.java
@@ -0,0 +1,37 @@
+package com.corewell.corewellmanage.constants;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: wangzhen
+ * @Date: 2022/11/30/9:51
+ * @Description:
+ */
+public class BaseRedisKeyConstants {
+    /**
+     * Redis普通键前缀
+     */
+    public static final String REDIS_KEY_PRE = "core:study:";
+
+    /**
+     * token键
+     */
+    public static final String ACCESS_TOKEN_KEY = REDIS_KEY_PRE + "access:" + "ACCESS_TOKEN";
+
+    /**
+     * 传感器名称键
+     */
+    public static final String SENSOR_KEY = REDIS_KEY_PRE + "sensor:";
+
+    /**
+     * 设备名称键
+     */
+    public static final String DEVICE_KEY = REDIS_KEY_PRE + "device:";
+
+    /**
+     * 设备在线名称键
+     */
+    public static final String DEVICE_IS_LINE_KEY = REDIS_KEY_PRE + "device:" + "isLine:";
+
+
+}
